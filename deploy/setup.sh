3af3d852a0baf0c9a5ec14dad58eca4a82d015e2
Osman Suliman <39956261+OzmanSolomon@users.noreply.github.com>
2022-11-29 19:39:43 +0400

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -16,6 +16,7 @@ echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
+rm -rf $PROJECT_BASE_PATH
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/profiles-rest-api
 
